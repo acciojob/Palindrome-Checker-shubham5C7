2ffff39f9fb9eac8bbaad2165b2dae40e5d980ff
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 11:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+let arr = str.split("");
+	let left = 0;
+	let right =  arr.length-1;
 
+	while(left <= right){
+		if(arr[left] != arr[right]){
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
 }
 module.exports = palindrome
