e826cc3c0660d355ce31e4f2925208f1ba6993dc
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-21 22:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function palindrome(str){
 	   let right = cleaned.length - 1;
 
 	while(left <= right){
-		if(cleaned[left] !== cleaned[right])){
+		if(cleaned[left] !== cleaned[right]) {
 			return false;
 		}
 		left++;
